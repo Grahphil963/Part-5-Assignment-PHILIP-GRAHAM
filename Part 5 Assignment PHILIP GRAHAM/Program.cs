5b14b4a44ff2ad74e5eb9a90b672627699f06a37
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-03-23 10:41:33 -0400

    I worked on my project

--- Part 5 Assignment PHILIP GRAHAM/Program.cs
+++ Part 5 Assignment PHILIP GRAHAM/Program.cs
@@ -13,7 +13,49 @@ namespace Part_5_Assignment_PHILIP_GRAHAM
             // Stages 
             Console.WriteLine("Please enter your age");
             int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+                Console.WriteLine("Adult");
+
+            else if (age <= 18 && age > 12)
+                Console.WriteLine("Teen");
+
+
+            else if (age <= 12 && age > 10)
+                Console.WriteLine("Preteen");
+
+
+            else if (age <= 10 && age > 5)
+                Console.WriteLine("Child");
+
+
+            else if (age <= 5 && age > 0)
+                Console.WriteLine("Toddler");
+
+            else if (age <= 0)
+                Console.WriteLine("Error");
+
+
+
+            //Hurricane 
             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
