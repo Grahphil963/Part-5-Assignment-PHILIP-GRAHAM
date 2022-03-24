5d5352499bf748408c73643453c09099f13cb0e0
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-03-24 10:59:35 -0400

    Im done working for the day

--- Part 5 Assignment PHILIP GRAHAM/Program.cs
+++ Part 5 Assignment PHILIP GRAHAM/Program.cs
@@ -10,7 +10,67 @@ namespace Part_5_Assignment_PHILIP_GRAHAM
     {
         static void Main(string[] args)
         {
-            // Stages 
+            //stages();
+
+
+
+
+
+
+
+            //Hurricane();
+          
+
+
+
+
+
+
+            /// Dice Game
+            int bankAccount = 100;
+
+
+            Console.WriteLine("Please enter what outcome you want to bet on (Doubles enter 1 to win 2x your bet) (Not doubles enter 2 to win 1.5x your bet) (Even Sum enter 3 win your bet) (Odd Sum enter 4 win your bet )");
+            int betRoll = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the amount you want to bet");
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        public static void stages()
+        {
             Console.WriteLine("Please enter your age");
             int age = Convert.ToInt32(Console.ReadLine());
             if (age > 18)
@@ -30,31 +90,30 @@ namespace Part_5_Assignment_PHILIP_GRAHAM
 
             else if (age <= 5 && age > 0)
                 Console.WriteLine("Toddler");
+        }
+        public static void Hurricane()
+        {
+            Console.WriteLine("Pleaase Enter a Hurricane Category (1-5)");
+            int category = Convert.ToInt32(Console.ReadLine());
+            switch (category)
+            {
+                case 1:
+                    Console.WriteLine($"The speed of the hurricane is between 74-95 mph");
+                    break;
+                case 2:
+                    Console.WriteLine($"The Speed of the hurricane is between 96-110 mph");
+                    break;
+                case 3:
+                    Console.WriteLine($"The Speed of the hurricane is between 111-130 mph");
+                    break;
+                case 4:
+                    Console.WriteLine($"The Speed of the hurricane is between 131-155 mph");
+                    break;
 
-            else if (age <= 0)
-                Console.WriteLine("Error");
-
-
-
-            //Hurricane 
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                case 5:
+                    Console.WriteLine($"The Speed of the hurricane is greater than 155 mph");
+                    break;
+            }
 
 
         }
